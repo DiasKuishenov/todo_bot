2b50779695d99716debc4046b539e1cded0707bf
XENO <255887@astanait.edu.kz>
2026-05-21 12:54:12 +0500

    Update

--- keyboards.py
+++ keyboards.py
@@ -4,8 +4,14 @@ from aiogram.types import KeyboardButton
 
 main_keyboard = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="/show")],
-        [KeyboardButton(text="/clear")]
+        [
+            KeyboardButton(text="/add"),
+            KeyboardButton(text="/show")
+        ],
+        [
+            KeyboardButton(text="/delete"),
+            KeyboardButton(text="/clear")
+        ]
     ],
     resize_keyboard=True
 )
